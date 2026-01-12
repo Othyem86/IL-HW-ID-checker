988c47cbd846f056987b52200a9b519242a987c0
Emil Föger <emil.foger@gmail.com>
2026-01-12 11:17:29 +0100

    Added license status to output

--- IL HW-ID Checker NET Fr 4.8/Program.cs
+++ IL HW-ID Checker NET Fr 4.8/Program.cs
@@ -1,5 +1,6 @@
 ﻿using IntelliLock.Licensing;
 using System;
+using System.ComponentModel;
 
 namespace IL_HW_ID_Checker_NET_Fr_4._8;
 
@@ -71,20 +72,49 @@ internal class Program
             BIOS:      true,
             OS:        true);
 
-        Console.WriteLine("=== HARDWARE IDs ===");
+        Console.WriteLine("=== HARDWARE IDs .NET Framework 4.8 ===");
         Console.WriteLine();
-        Console.WriteLine($"BIOS:           {biosHwId}");
-        Console.WriteLine($"CPU:            {cpuHwId}");
-        Console.WriteLine($"HDD:            {hddHwId}");
-        Console.WriteLine($"MAC:            {macHwId}");
-        Console.WriteLine($"Mainboard:      {maiboardHwId}");
-        Console.WriteLine($"OS:             {osHwId}");
+        Console.WriteLine($"BIOS:               {biosHwId}");
+        Console.WriteLine($"CPU:                {cpuHwId}");
+        Console.WriteLine($"HDD:                {hddHwId}");
+        Console.WriteLine($"MAC:                {macHwId}");
+        Console.WriteLine($"Mainboard:          {maiboardHwId}");
+        Console.WriteLine($"OS:                 {osHwId}");
 
         Console.WriteLine();
-        Console.WriteLine($"CPU/Mainboard:  {cpuMainboardHwId}");
-        Console.WriteLine($"FULL:           {fullHwId}");
+        Console.WriteLine($"CPU/Mainboard:      {cpuMainboardHwId}");
+        Console.WriteLine($"FULL:               {fullHwId}");
+
+        var licenseStatus = GetLicenseStatus(EvaluationMonitor.CurrentLicense.LicenseStatus);
+        var licenseStatusHdd = GetLicenseStatus(EvaluationMonitor.CurrentLicense.LicenseStatus_HDD);
+
+        Console.WriteLine();
+        Console.WriteLine($"License Status:     {licenseStatus}");
+        Console.WriteLine($"License Status HDD: {licenseStatusHdd}");
 
         Console.WriteLine("Press any key to end the program...");
         Console.ReadLine();
+    }
+
+    private static string GetLicenseStatus(LicenseStatus status)
+    {
+        return status switch
+        {
+            LicenseStatus.NotChecked => "NotChecked",
+            LicenseStatus.Licensed => "Licensed",
+            LicenseStatus.EvaluationMode => "EvaluationMode",
+            LicenseStatus.EvaluationExpired => "EvaluationExpired",
+            LicenseStatus.LicenseFileNotFound => "LicenseFileNotFound",
+            LicenseStatus.HardwareNotMatched => "HardwareNotMatched",
+            LicenseStatus.InvalidSignature => "InvalidSignature",
+            LicenseStatus.ServerValidationFailed => "ServerValidationFailed",
+            LicenseStatus.Deactivated => "Deactivated",
+            LicenseStatus.Reactivated => "Reactivated",
+            LicenseStatus.FloatingLicenseUserExceeded => "FloatingLicenseUserExceeded",
+            LicenseStatus.FloatingLicenseServerError => "FloatingLicenseServerError",
+            LicenseStatus.FullVersionExpired => "FullVersionExpired",
+            LicenseStatus.FloatingLicenseServerTimeout => "FloatingLicenseServerTimeout",
+            _ => "INVALID LICENSE STATUS"
+        };
     }
 }
